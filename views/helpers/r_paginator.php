f5f93f7941deae30812f521500b27a30ca761e4a
Cauan Cabral <cauan@radig.com.br>
2011-06-29 16:48:09 -0400

    Correção de bug que sobrescrevia opções do paginador

--- views/helpers/r_paginator.php
+++ views/helpers/r_paginator.php
@@ -1,15 +1,15 @@
 <?php
-/** 
+/**
  * Helper para encapsular lógica de paginação
- * 
+ *
  * Possui método para imprimir/retornar todos os campos de paginação de uma única vez,
  * adiciona a url dos links de paginação as variáveis passadas via url a ação atual.
- * 
+ *
  * PHP version 5
- * 
+ *
  * Licensed under The MIT License
  * Redistributions of files must retain the above copyright notice.
- * 
+ *
  * @filesource
  * @copyright	Copyright 2011, Radig Soluções em TI. (http://www.radig.com.br)
  * @link        http://www.radig.com.br
@@ -23,19 +23,19 @@ class RPaginatorHelper extends AppHelper
 {
 	/**
 	 * Referência para instância do PaginatorHelper
-	 * 
+	 *
 	 * @var PaginatorHelper
 	 */
 	protected $_paginator = null;
-	
+
 	/**
 	 * Prepara e retorna/imprime os controles de navegação
-	 * 
+	 *
 	 * O primeiro parâmetro corresponde a uma referência a classe PaginatorHelper
 	 * instanciada pelo controlador, enquanto o segundo parâmetro é um array com as opções
 	 * de saída.
-	 * 
-	 * 
+	 *
+	 *
 	 * @param PaginatorHelper $paginator
 	 * @param array $options Possui dois índices reconhecidos:
 	 *  - 'return' do tipo bool, se true faz o método retornar o resultado ao invés de imprimir.
@@ -46,22 +46,26 @@ class RPaginatorHelper extends AppHelper
 	public function controls(&$paginator, $options = array())
 	{
 		$this->_paginator =& $paginator;
-		
+
 		if(!empty($options['filters']) && is_array($options['filters']))
 		{
-			$this->_paginator->options(array('url' => array_merge($options['filters'], $this->params['pass'])));
+			$filters['url'] = array_merge($options['filters'], $this->params['pass']);
 		}
 		else
 		{
-			$this->_paginator->options(array('url' => $this->params['pass']));
+			$filters['url'] = $this->params['pass'];
 		}
-		
+
+		$options = Set::merge($filters, $this->_paginator->options);
+
+		$this->_paginator->options($options);
+
 		$out =	$this->_paginator->first('<< '.__d('r_search', 'first', true), array(), null, array('class'=>'disabled')) .
 				$this->_paginator->prev(__d('r_search', 'previous', true), array(), null, array('class'=>'disabled')) .
 				$this->_paginator->numbers(array('separator' => '')) .
 				$this->_paginator->next(__d('r_search', 'next', true), array(), null, array('class' => 'disabled')) .
 				$this->_paginator->last(__d('r_search', 'last', true).' >>', array(), null, array('class' => 'disabled'));
-		
+
 		if(isset($options['return']) && $options['return'] === true)
 		{
 			return $out;
